2a0b691f9d4e8fff3fb2a120498039464fb731e8
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-25 06:12:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(input)
-  if input % 3 == 1
+  if input % 3 == 0
     return "Fizz"
-  elsif input % 5 == 1
+  elsif input % 5 == 0
     puts "Buzz"
-  elsif input % 3 == 1 && input % 5 == 1
+  elsif input % 3 == 0 && input % 5 == 0
     puts "FizzBuzz"
   else
     return input = nil
